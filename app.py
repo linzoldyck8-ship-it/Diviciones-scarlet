ceae9b04ddc6e3a06191b9700d19f63742e5da5b
linzoldyck8-ship-it <linzoldyck8@gmail.com>
2026-09-20 03:26:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -735,7 +735,9 @@ elif st.session_state.menu_activo == "Tracker":
                 
         st.markdown(f"**Captura de Rendimiento — {nick_seleccionado}**")
         img_upload = st.file_uploader("Cargar captura", type=["png", "jpg", "jpeg"])
-     if img_upload:st.image(img_upload, use_container_width=True, caption=f"Registro analítico para {nick_seleccionado}")
+        if img_upload: 
+            st.image(img_upload, use_container_width=True, caption=f"Registro analítico para {nick_seleccionado}")
+
 
 # ==========================================
 # SECCIÓN 5: CONFIGURACIÓN Y BORRADOS (SOLO ADMIN)
